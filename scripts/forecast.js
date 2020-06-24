599a73ac462600f49c41de0af9150da087a5d422
hiteshbhargav <47861535+hiteshbhargav@users.noreply.github.com>
2020-06-24 11:31:14 +0530

    update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@
 const key = "BFLSTGVDdT3BeANUm7JyDH5pFqIWu9Ih";
 // get city information
 const getCity = async (city) => {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
+  const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`;
 
   const response = await fetch(url);
   const data = await response.json();
@@ -11,7 +11,7 @@ const getCity = async (city) => {
 
 //get weather from city code
 const getWeather = async (cityCode) => {
-  const url = `http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${key}`;
+  const url = `https://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=${key}`;
 
   const response = await fetch(url);
   const data = await response.json();
